1b9482a0332e0a764046017c2e203484b4767e3a
linkin6713 <hoangvhh@gmail.com>
2017-06-17 22:39:05 +0700

    + Ađ snap carousel

--- app/src/screens/SplashScreen.js
+++ app/src/screens/SplashScreen.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {DNPageRoute} from "../NavigationHelper";
-import {HomeScreen} from "./HomeScreen";
-import {style} from "../styles/style";
-
+import {StyleSheet, Text, View} from 'react-native';
+import {DNPageRoute} from '../NavigationHelper';
+import {HomeScreen} from './HomeScreen';
+import {style} from '../styles/style';
+import SnapCarousel from '../components/snap-carousel';
 export class SplashScreen extends React.Component {
   componentWillMount() {
-    setTimeout(() => {
-      this.goNext();
-    }, 1000);
+
   }
 
   render() {
     return (
       <View style={style.container}>
-        <Text>Splash Screen Here</Text>
+        <SnapCarousel />
       </View>
     )
   }
