9dfad5efe1310a81a1e4ab14a69ccaca76db175a
linkin6713 <hoangvhh@gmail.com>
2017-07-25 23:04:00 +0700

    ! Update build android

--- app/android/app/src/main/java/com/gotodn/MainApplication.java
+++ app/android/app/src/main/java/com/gotodn/MainApplication.java
@@ -3,13 +3,13 @@ package com.gotodn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
             new VectorIconsPackage(),
+            new MapsPackage(),
             new LocationServicesDialogBoxPackage()
       );
     }
