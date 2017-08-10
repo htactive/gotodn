fe8228f624d618b2f98516bb752497400ba2fbc7
Huan <ndthuan2266@gmail.com>
2017-08-10 16:00:11 +0700

    fix

--- app/src/services/GDNService.js
+++ app/src/services/GDNService.js
@@ -3,7 +3,7 @@ import {timeout, MenuListData} from '../common/DummyData';
 import {Helper, LanguageEnums, IconName} from '../common/constain';
 import {appStore} from '../stores/AppStore';
 class GDNService extends ServiceBase {
-  host = "http://192.168.1.8:50915/";
+  host = "http://gdn.htactive.com/";
 
   async getHomeSlider() {
     let url = this.host + "category/get-category-slider";
